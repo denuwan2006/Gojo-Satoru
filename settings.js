25a81483abc4158b54c2f26b1fe91ba53f4bc1c9
denuwan2006 <109402952+denuwan2006@users.noreply.github.com>
2022-09-11 17:53:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94773596116']
+global.premium = ['94773596116']
+global.ownernomer = '94773596116'
+global.ownername = 'Blacky 💗'
 global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.footer = 'blacky💗'
+global.ig = 'https://www.instagram.com/invites/contact/?i=1whphec1rj7l9&utm_content=mtswn5h
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'blacky'
+global.author = 'blacky💗'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
